ca4a598400a99b753f4056bf97b882ebe67cc830
Brydon Parker <brydon.parker@shopify.com>
2022-03-19 17:30:32 -0400

    fixing code checking

--- app.py
+++ app.py
@@ -5,15 +5,15 @@ AUTH_ENDPOINT = "https://shopify-streamlit.herokuapp.com/dash_auth"
 query_params = st.experimental_get_query_params()
 
 def authenticate(shop:str, state:str):
-    r = requests.get(
+    response = requests.get(
         AUTH_ENDPOINT, params={"shop": shop[0], "state": state[0]}, timeout=5001
     )
-    return r
+    return response
 
 st.title(":fire: ShopLit :fire:")
-auth_code = authenticate(query_params['shop'], query_params['state'])
-print(auth_code)
-if auth_code == 200:
+response = authenticate(query_params['shop'][0], query_params['state'][0])
+print(response)
+if response.status_code == 200:
     st.write("Success")
 else:
     st.write('Please Access This App Through Your Shopify Admin')

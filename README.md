b2cf18eae3e89458b108af973ce33ac0689f5c83
Brydon Parker <brydonparker4@gmail.com>
2022-03-20 15:49:02 -0400

    New/reading orders (#2)
    * v1 auth in app
    
    * showing the os.environ
    
    * using st.experimental_get_query_params()
    
    * readme updates
    
    * printing error codes
    
    * printing auth code
    
    * fixing code checking
    
    * fixing param passing to requests
    
    * showing error message for those who try and access the url directly
    
    * reading orders
    
    * updating req.txt
    
    * updating requirements.txt
    
    * changing from st.table to st.dataframe
    
    * adding a description for the table
    
    * bug fix in query from orders table

--- README.md
+++ README.md
@@ -3,7 +3,9 @@ Example Streamlit Dashboard for a Shopify App.
 
 See the rest of the Shopify App here: https://github.com/parker84/shopify-streamlit-example
 
-## Heroku Setup
+## Setup
+
+### Step 1: Setup this App
 ```sh
 heroku create shoplit-dash
 heroku git:remote -a shoplit-dash
@@ -13,3 +15,17 @@ git push heroku new_branch:main
 # for deploying the dynos initially
 heroku ps:scale web=1
 ```
+
+### Step 2: Connect to the DB
+The db that you made in [step 4 here](https://github.com/parker84/shopify-streamlit-example#step-4-get-your-db-setup-and-start-retrieving-order-data).
+
+Set your db parameters as environmental variables in heroku
+```sh
+heroku config:set DB_HOST=db_host
+heroku config:set DB=db_name
+heroku config:set DB_USER=db_user_name
+heroku config:set DB_PWD=db_password
+# port is assumed to be 5432
+```
+
+If these are set we will try and grab the first 20 rows and show them.
--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 import requests
 import streamlit as st
+import os
+from sqlalchemy import create_engine
+import pandas as pd
+
+if os.environ.get('DB_HOST') is not None:
+    ENGINE_PATH = f"postgresql://{os.environ.get('DB_USER')}:{os.environ.get('DB_PWD')}@{os.environ.get('DB_HOST')}/{os.environ.get('DB')}"
+    engine = create_engine(ENGINE_PATH)
+    conn = engine.connect()
+else:
+    ENGINE_PATH = None
 
 AUTH_ENDPOINT = "https://shopify-streamlit.herokuapp.com/dash_auth"
 query_params = st.experimental_get_query_params()
@@ -14,9 +24,14 @@ st.title(":fire: ShopLit :fire:")
 if 'shop' not in query_params:
     st.write('Please Access This App Through Your Shopify Admin')
 else:
-    response = authenticate(query_params['shop'][0], query_params['state'][0])
+    shop, state = query_params['shop'][0], query_params['state'][0]
+    response = authenticate(shop, state)
     print(response)
     if response.status_code == 200:
         st.write("Success")
+        if ENGINE_PATH is not None:
+            st.write("Here's 20 orders from your shop:")
+            df = pd.read_sql(f'select * from orders_{shop.replace(".", "_")} limit 20', con=conn)
+            st.dataframe(df)
     else:
         st.write('Please Access This App Through Your Shopify Admin')
